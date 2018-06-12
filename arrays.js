0ebb34c1f7d11088b17e180d3a25d9ed88f0b790
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-12 20:53:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function destructivelyAddElementToEndOfArray (array, element){
 }
 
 function accessElementInArray (array, index){
-  return console.log(array[index]);
+  return array[index];
 }
 
 
