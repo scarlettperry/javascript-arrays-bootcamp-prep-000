b192c9f0259459f9a9ec31d1e1aad17f6fa7d3fa
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-12 21:05:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,8 @@ function destructivelyRemoveElementFromEndOfArray (array){
 }
 
 function removeElementFromEndOfArray (array){
-  
+  array = array.slice(0, array.length-1);
+  return array;
 }
 
 
