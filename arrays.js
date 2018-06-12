a7662a49f39f90c18b91da002a3cad8cb09376b4
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-12 20:54:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,7 +24,10 @@ function accessElementInArray (array, index){
   return array[index];
 }
 
-function 
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  array.shift();
+  return array;
+}
 
 
 
