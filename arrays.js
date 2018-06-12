bb2b5ac9e0a9a1adafd1cd74d26b92431ba0c29a
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-12 20:28:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, item) {
   
+  
 }
 
 function destructivelyAddElementToBeginningOfArray (array, item) {
