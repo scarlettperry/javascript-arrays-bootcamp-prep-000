a60d84fa88c366813dece3e956300df22cb3f533
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-12 21:02:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 }
 
 function removeElementFromBeginningOfArray (array){
-array = array.slice(0);
+array = array.slice(1);
 return array;
 }
 
